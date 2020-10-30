f817b0d10d5488e1aa5b6814bce4b74a4dff32a5
jphillip44 <jphillip44@github.com>
2020-10-29 22:35:15 -0400

    reorganized main class

--- bot/main.py
+++ bot/main.py
@@ -3,13 +3,12 @@ from discord.ext import commands
 
 import config
 
-
-intents = discord.Intents(messages=True, guilds=True, reactions=True, members=True, presences=True)
-client = commands.Bot(command_prefix='!', intents=intents)
-
 @client.event
 async def on_ready():
     print('Ready!')
 
 if __name__ == "__main__":
+    intents = discord.Intents(messages=True, guilds=True, reactions=True, members=True, presences=True)
+    client = commands.Bot(command_prefix='!', intents=intents)
+
     client.run(config.TOKEN)
