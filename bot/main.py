b7e5f8cebc2fcd9923943f8c77066428566d4c0c
jphillip44 <jphillip44@gmail.com>
2020-10-31 22:02:46 -0400

    added fuzzy matching

--- bot/main.py
+++ bot/main.py
@@ -1,9 +1,11 @@
 import discord
 from discord.ext import commands
 from discord.utils import get
+from fuzzywuzzy import fuzz
+
 
 import config
-from utils import to_upper, to_lower, to_cap, to_title
+# from utils import to_upper, to_lower, to_cap, to_title
 
 intents = discord.Intents(messages=True, guilds=True, reactions=True, members=True, presences=True)
 client = commands.Bot(command_prefix='!', intents=intents)
@@ -17,26 +19,17 @@ async def verify(context):
     if context.message.author == client.user:
         return
 
-    async def assign_role(requested, func, case):
-        if func(requested) in case:
-            await context.message.author.add_roles(get(context.message.author.guild.roles, name=func(requested)))
-
-            print('Role assigned.')
-
     if context.message.channel.name == config.verification_channel:
-        roles = { role.name for role in client.get_guild(int(config.server_id)).roles }
+        roles = { role.name.lower() : role.name for role in client.get_guild(int(config.server_id)).roles }
         requestedRoles = context.message.content.split()
 
-        upper = { role for role in roles if role.isupper() }
-        lower = { role for role in roles if role.islower() }
-        cap = { role for role in roles if role[0].isupper() and role[1:].islower() }
-        title = { role for role in roles if role.title() }
 
         for requested in requestedRoles:
-            await assign_role(requested, to_upper, upper)
-            await assign_role(requested, to_lower, lower)
-            await assign_role(requested, to_cap, cap)
-            await assign_role(requested, to_title, title)
+            req = max(((ratio, role) for role in roles if (ratio := fuzz.partial_ratio(role, requested.lower())) > 75), default=None)
+            if req is not None:
+                await context.message.author.add_roles(get(context.message.author.guild.roles, name=roles[req[1]]))
+
+                print('Role assigned.')
 
 if __name__ == "__main__":
     client.run(config.token)
