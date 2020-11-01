bb3ac2aa9b156ae8b84e53abe0e49c1b29bcead5
Abdul Haseeb <ahaseeb0104@gmail.com>
2020-11-01 00:26:23 -0400

    Wait for mod to approve

--- bot/main.py
+++ bot/main.py
@@ -1,7 +1,8 @@
+import asyncio
 import discord
-from discord.ext import commands
-from discord.utils import get
 from fuzzywuzzy import fuzz
+from discord.utils import get
+from discord.ext import commands
 
 import config
 
@@ -17,20 +18,26 @@ async def verify(context):
     if context.message.author == client.user:
         return
 
+    def check(reaction, user):
+        return str(reaction.emoji) == '👍' and user.guild_permissions.manage_roles
+
     if context.message.channel.name == config.verification_channel:
         roles = { role.name.lower() : role.name for role in client.get_guild(int(config.server_id)).roles }
-        requestedRoles = context.message.content.split(';')
+        requestedRoles = [ role.strip() for role in context.message.content.split(';') ]
 
         for requestedRole in requestedRoles:
-            if requestedRole[-1:].isspace():
-                requestedRole = requestedRole[:-1]
-
             requested = max(((ratio, role) for role in roles if (ratio := fuzz.partial_ratio(role, requestedRole.lower())) > 70), default=None)
 
             if requested is not None:
-                await context.message.author.add_roles(get(context.message.author.guild.roles, name=roles[requested[1]]))
+                try:
+                    reaction, user = await client.wait_for('reaction_add', check=check)
+                except asyncio.TimeoutError as e:
+                    print(e)
+                else:
+                    await context.message.author.add_roles(get(context.message.author.guild.roles, name=roles[requested[1]]))
 
-                print('Role assigned.')
+                    print('Role assigned.')
+
 
 if __name__ == "__main__":
     client.run(config.token)
