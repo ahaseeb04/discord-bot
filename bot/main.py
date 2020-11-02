63527755c4e46c314dfe8bbbcc1c1a08217377e0
Abdul Haseeb <ahaseeb0104@gmail.com>
2020-11-02 17:03:51 -0500

    Added cogs

--- bot/main.py
+++ bot/main.py
@@ -1,9 +1,6 @@
-import asyncio
-import csv
+import os
 
 import discord
-from fuzzywuzzy import fuzz
-from discord.utils import get
 from discord.ext import commands
 
 import config
@@ -15,45 +12,9 @@ client = commands.Bot(command_prefix=';', intents=intents)
 async def on_ready():
     print('Ready!')
 
-@client.command()
-async def verify(context):
-    def check_reaction(message):
-        def check(reaction, user):
-            return reaction.message.id == message.id \
-                and reaction.emoji == '👍' \
-                and user.guild_permissions.manage_roles \
-                and str(user.id) != config.bot_id
-
-        return check
-
-    def get_requested_roles():
-        requested_roles = [ role.strip() for role in context.message.content.split(';') ]
-
-        for requested_role in requested_roles:
-            requested = max(((ratio, role) for role in roles if (ratio := fuzz.token_sort_ratio(role, requested_role)) > 70), default=None)
-            if requested is None:
-                requested = max(((ratio, role) for role in roles if (ratio := fuzz.partial_ratio(role, requested_role.lower())) > 70), default=None)
-            yield requested
-
-    if context.message.channel.name == config.verification_channel:
-        try:
-            roles = { role.name.lower() : role.name for role in client.get_guild(int(config.server_id)).roles }
-            roles = { **roles, **aliases }
-
-            requested_roles = list(get_requested_roles())
-
-            await context.message.add_reaction(emoji='👍')
-            await client.wait_for('reaction_add', check=check_reaction(context.message))
-        except asyncio.TimeoutError as e:
-            pass
-        else:
-            for requested in requested_roles:
-                if requested is not None and len(role := roles.get(requested[1])):
-                    await context.message.author.add_roles(get(context.message.author.guild.roles, name=role))
-
-                    print(f'{role} role assigned.')
-
 if __name__ == "__main__":
-    aliases = { key : value.strip() for key, value in csv.reader(open('bot/aliases.csv', 'r')) }
+    for cog in os.listdir('bot/cogs'):
+        if cog != '__pycache__':
+            client.load_extension(f'cogs.{cog[:-3]}')
 
     client.run(config.token)
