74e9afae1c18fd6c7eef59c08e033baeb15fa6e0
Abdul Haseeb <ahaseeb0104@gmail.com>
2020-11-01 19:14:57 -0500

    abc

--- bot/main.py
+++ bot/main.py
@@ -17,30 +17,30 @@ async def on_ready():
 
 @client.command()
 async def verify(context):
-    if context.message.author == client.user:
-        return
-
-    def react_check(message):
+    def check_reaction(message):
         def check(reaction, user):
             return reaction.message.id == message.id \
                 and reaction.emoji == '👍' \
                 and user.guild_permissions.manage_roles \
                 and str(user.id) != config.bot_id
+
         return check
 
     if context.message.channel.name == config.verification_channel:
         try:
             await context.message.add_reaction(emoji='👍')
-            await client.wait_for('reaction_add', check=react_check(context.message))
+            await client.wait_for('reaction_add', check=check_reaction(context.message))
         except asyncio.TimeoutError as e:
-            print(e)
+            pass
         else:
             roles = { role.name.lower() : role.name for role in client.get_guild(int(config.server_id)).roles }
             roles = { **roles, **aliases }
+
             requested_roles = [ role.strip() for role in context.message.content.split(';') ]
 
             for requested_role in requested_roles:
                 requested = max(((ratio, role) for role in roles if (ratio := fuzz.token_sort_ratio(role, requested_role)) > 70), default=None)
+
                 if requested is None:
                     requested = max(((ratio, role) for role in roles if (ratio := fuzz.partial_ratio(role, requested_role.lower())) > 70), default=None)
 
@@ -50,6 +50,6 @@ async def verify(context):
                     print(f'{role} role assigned.')
 
 if __name__ == "__main__":
-    aliases = { key :  val.strip() for key, val in csv.reader(open('bot/aliases.csv', 'r')) }
+    aliases = { key : value.strip() for key, value in csv.reader(open('bot/aliases.csv', 'r')) }
 
     client.run(config.token)
