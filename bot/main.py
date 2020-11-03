62ef687069832da3b61d2e316fff989c578ecddd
jphillip44 <jphillip44@gmail.com>
2020-11-03 01:39:17 -0500

    cogs is no longer reading from directory

--- bot/main.py
+++ bot/main.py
@@ -1,9 +1,11 @@
 import os
+import inspect
 
 import discord
 from discord.ext import commands
 
 import config
+import cogs
 
 intents = discord.Intents(messages=True, guilds=True, reactions=True, members=True, presences=True)
 client = commands.Bot(command_prefix=';', intents=intents)
@@ -13,8 +15,8 @@ async def on_ready():
     print('Ready.')
 
 if __name__ == "__main__":
-    for cog in os.listdir('bot/cogs'):
-        if cog != '__pycache__':
-            client.load_extension(f'cogs.{cog[:-3]}')
+    for cog in inspect.getmembers(cogs, inspect.isclass):
+        print(cog)
+        client.load_extension(f'cogs.{cog[1].__module__}')
 
     client.run(config.token)
