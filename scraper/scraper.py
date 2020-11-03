791d9d4968a775a81173d63d41169fff161e89a6
jphillip44 <jphillip44@gmail.com>
2020-11-02 20:37:11 -0500

    scraper now pulls title/desc and outputs to a dict

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,39 +1,66 @@
+from itertools import islice
+
 import requests
 import bs4
-
-# course = ["LE", "EECS", "3101"]
-course = ["SC", "PHYS", "1411"]
-
-URL = f"https://w2prod.sis.yorku.ca/Apps/WebObjects/cdm.woa/wa/crsq1?faculty={course[0]}&subject={course[1]}&academicyear=2020&studysession=FW"
-page = requests.get(URL)
-
-soup = bs4.BeautifulSoup(page.content, 'html.parser')
-rows = soup.find_all('table')[6].find_all('tr')
   
-def lookup(rows, course_code):
+def find_course_URL(course):
+    URL = f"https://w2prod.sis.yorku.ca/Apps/WebObjects/cdm.woa/wa/crsq1?faculty={course[0]}&subject={course[1]}&academicyear=2020&studysession=FW"
+    page = requests.get(URL)
+
+    soup = bs4.BeautifulSoup(page.content, 'html.parser')
+    rows = soup.find_all('table')[6].find_all('tr', recursive=False)
+
     for row in rows[1:]:
-        code, link = row.find_all('td')[::2]
-        if code.text.split()[1] == course_code:
+        code, link = row.find_all('td', recursive=False)[::2]
+        if code.text.split()[1] == course[2]:
             return ''.join(("https://w2prod.sis.yorku.ca/", link.find('a')['href']))
 
-URL = lookup(rows, course[2])
-page = requests.get(URL)
-soup = bs4.BeautifulSoup(page.content, 'html.parser')
+def scrape_course(course):
+    def scrape_heading(soup):
+        return soup.find(class_="heading").text
 
-courses = [section for section in soup.find_all('table')[6] if isinstance(section, bs4.element.Tag)]
+    def scrape_description(soup):
+        return soup.find_all('table')[2].find_all('p')[3].text
 
-for section in courses:
-    info = section.find('tr').text.strip()
-    print(info)
-    rows = section.find_all('tr')[2].find('table')
-    for row in list(rows)[1:]:
-        children = list(row.children)
-        instructor = ', '.join(c.text for c in children[3].find_all('a'))
-        lect = children[0].text
-        times = ' '.join(c.text for c in children[1].find_all('td'))
-        if children[2].text == 'Cancelled':
-            print('Cancelled')
-        if len(instructor) > 1:
-            print("Instructor:", instructor)
-        if len(times):
-            print(lect, times, sep=': ')
+    def scrape_section(section):
+        sect = {}
+        info = ' '.join(section.tr.stripped_strings)
+        rows = section.find_all('tr')[2].table
+        labels = [r.text for r in rows.td.next_sibling.find_all('b')]
+
+        sect[info] = {}
+        for row in islice(rows, 1, None):
+            columns = row.find_all('td', recursive=False)
+            if columns[2].text == 'Cancelled':
+                sect['cancelled'] = 'Cancelled'
+            else:
+                lect_type = columns[0].text
+                sect[info][lect_type] = {}
+                instructor = ', '.join(c.text for c in row.find_all('a'))
+                if len(instructor) > 1:
+                    sect[info][lect_type]['instructors'] = instructor
+                sect[info][lect_type]['lectures'] = []
+                for row in columns[1].find_all('tr'):
+                    lec = { l : r.text for l, r in zip(labels, row) }
+                    sect[info][lect_type]['lectures'].append(lec)
+        return sect
+        
+
+    URL = find_course_URL(course)
+    page = requests.get(URL)
+    soup = bs4.BeautifulSoup(page.content, 'html.parser')
+
+    crs = {}
+    crs['heading'] = scrape_heading(soup)
+    crs['description'] = scrape_description(soup)
+    sections = filter(lambda s: isinstance(s, bs4.element.Tag), soup.find_all('table')[6])
+    crs['sections'] = list(map(scrape_section, sections))
+
+    return crs
+    
+if __name__ == "__main__":
+    course = ["LE", "EECS", "3101"]
+    # course = ["SC", "PHYS", "1421"]
+    # course = ["AP", "ECON", "1000"]
+
+    print(scrape_course(course))
