b37cf72fcbae7e7741f07272df9f9ea98fc1630b
Abdul Haseeb <ahaseeb0104@gmail.com>
2020-11-14 00:02:14 -0500

    Send hey in engineering channel

--- bot/cogs/misc.py
+++ bot/cogs/misc.py
@@ -30,6 +30,14 @@ class DailyReminder(_Cog, name='js'):
     async def daily_reminder(self):
         await self.client.get_channel(int(config.cs_channel)).send("Daily reminder js is ass")
 
+class Hey(_Cog, name='hey'):
+    def __init__(self, client):
+        _Cog.__init__(self, client)
+        aiocron.crontab('0 10 * * *', func=self.daily_hey, start=True, tz=timezone('US/Eastern'))
+
+    async def daily_hey(self):
+        await self.client.get_channel(int(config.engineering_channel)).send("Hey")
+
 class StfuuuuuAunk(_Cog):
     @_Cog.listener(name='on_message')
     async def stfuuuuu_aunk(self, message):
