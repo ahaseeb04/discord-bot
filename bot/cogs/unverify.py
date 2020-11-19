f15be4e1c51028c612c32b660acd4d8b4c484410
jphillip44 <jphillip44@gmail.com>
2020-11-19 15:59:03 -0500

    added logging

--- bot/cogs/unverify.py
+++ bot/cogs/unverify.py
@@ -43,6 +43,7 @@ class Unverify(_Cog):
     @commands.command(hidden=True)
     async def get_df(self, context):
         self.df = sql_to_df('last message', self.engine, 'user id')
+        print(self.df)
         await context.channel.send(f"```\n{tabulate(self.df, headers='keys', tablefmt='psql')}```")
     
     @commands.has_permissions(manage_roles=True)
@@ -50,6 +51,7 @@ class Unverify(_Cog):
     async def get_redis(self, context):
         data = self.redis.hgetall("users")
         df = pd.DataFrame.from_dict(data, orient='index', columns=['last message']).rename_axis('user id')
+        print(df)
         await context.channel.send(f"```\n{tabulate(df, headers='keys', tablefmt='psql')}```")
 
     @commands.has_permissions(manage_roles=True)
@@ -62,5 +64,6 @@ class Unverify(_Cog):
         df = pd.DataFrame.from_dict(data, orient='index', columns=['verified', 'last message']).rename_axis('user id')
         df.update(self.df)
         df_to_sql(df, 'last message', self.engine)
+        print(df)
 
     
