7d3d66d60271e493aef92336daed7fca53810af5
jphillip44 <jphillip44@gmail.com>
2020-11-20 15:07:12 -0500

    scraper no longer crashes on missing fields

--- bot/cogs/rmp.py
+++ bot/cogs/rmp.py
@@ -13,9 +13,9 @@ class RMP(_Cog, name='rmp'):
         professor_name = context.message.content.lower().split()[1:]
 
         try:
-            professors, c = tee(scrape_rmp(professor_name))
+            professors, test = tee(scrape_rmp(professor_name))
 
-            if next(c, None) is None:
+            if next(test, None) is None:
                 raise DataNotFoundError()
         except DataNotFoundError:
             await context.message.channel.send('**Error**: Sorry, could not find professor!')
@@ -26,10 +26,12 @@ class RMP(_Cog, name='rmp'):
                 embed = discord.Embed(title=professor['name'], description=professor['description'], url=professor['url'], color=0x1d4ed8)
                 embed.set_thumbnail(url='https://i.imgur.com/dwlne0a.png')
 
-                embed.add_field(name='Top review', value=professor['top_review'], inline=False)
-                embed.add_field(name='Rating', value=professor['rating'] + '/5', inline=False)
-                embed.add_field(name='Difficulty', value=professor['difficulty'] + '/5', inline=False)
-                embed.add_field(name='Retake percentage', value=professor['retake_percentage'], inline=False)
+                if professor['top_review'] is not None:
+                    embed.add_field(name='Top review', value=professor['top_review'], inline=False)
+                if professor['rating'] != 'N/A':
+                    embed.add_field(name='Rating', value=professor['rating'], inline=False)
+                for label, rating in professor['feedback']:
+                    embed.add_field(name=label, value=rating, inline=False)
 
                 embeds.append(embed)
 
