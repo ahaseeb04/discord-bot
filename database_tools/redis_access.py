193f960a0edb72fede51872453dc8f32d5e338ac
jphillip44 <jphillip44@gmail.com>
2020-11-23 13:59:22 -0500

    deverify working

--- database_tools/redis_access.py
+++ database_tools/redis_access.py
@@ -4,4 +4,4 @@ import redis
 def redis_access(url=None, params=None):
     if url:
         return redis.StrictRedis.from_url(url, decode_responses=True)
-    return redis.StrictRedis(decode_responses=True, db=0, **params, )
+    return redis.StrictRedis(decode_responses=True, db=0, **params)
