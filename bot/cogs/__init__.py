e72f2f78ce0db2444eee4ab28724d34bbc6f5490
Abdul Haseeb <ahaseeb0104@gmail.com>
2020-11-26 02:18:14 -0500

    Added a command to fetch information about user

--- bot/cogs/__init__.py
+++ bot/cogs/__init__.py
@@ -2,6 +2,7 @@ from .verify_user import VerifyUser
 from .course import Course
 from .course_list import CourseList
 from .rmp import RMP
+from .user import User
 from .misc import CronJobs, StfuuuuuAunk, Main
 from .aliases import Aliases
 from .last_message import LastMessage
