803da4eed26fce95471254de60daa85eb6539395
Abdul Haseeb <ahaseeb0104@gmail.com>
2020-11-26 04:46:05 -0500

    Added a default color

--- bot/utils.py
+++ bot/utils.py
@@ -1,7 +1,10 @@
 import discord
 
 async def get_user(context, user):
-    embed = discord.Embed(description=user.mention, color=user.color)
+    roles = [ role.mention for role in user.roles if role.name != '@everyone' ]
+    color = user.color if len(roles) > 0 or str(user.color) != '#000000' else 0xdb2777
+
+    embed = discord.Embed(description=user.mention, color=color)
 
     embed.set_author(name=f'{user.name}#{user.discriminator}', icon_url=user.avatar_url)
     embed.set_thumbnail(url=user.avatar_url)
@@ -9,7 +12,6 @@ async def get_user(context, user):
     embed.add_field(name='Registered', value=user.created_at.strftime("%c"), inline=True)
     embed.add_field(name='Joined at', value=user.joined_at.strftime("%c"), inline=True)
 
-    roles = [ role.mention for role in user.roles if role.name != '@everyone' ]
     if len(roles) > 0:
         embed.add_field(name=f'Roles [{len(roles)}]', value=', '.join(roles), inline=False)
 
