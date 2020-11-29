824688eef34888d9f7fe63c98c12751ce1066ee9
jphillip44 <jphillip44@gmail.com>
2020-11-29 16:08:03 -0500

    user now types me to get their own stats

--- bot/cogs/user.py
+++ bot/cogs/user.py
@@ -6,20 +6,19 @@ from bot.exceptions import DataNotFoundError, IllegalFormatError
 from ._cog import _Cog
 
 class User(_Cog, name="user"):
-    @commands.command(brief='Fetch details for a user.', aliases=['member', 'u', 'whois'])
+    @commands.command(brief='Fetch details for a spcified user.', aliases=['member', 'u', 'whois'])
     async def user(self, context):
         try:
-            if len(context.message.content.split()) == 1:
-                raise IllegalFormatError()
-
-            if not len(context.message.mentions):
-                raise DataNotFoundError()
-
             embeds = [get_user(context, user) for user in context.message.mentions]
-
-        except IllegalFormatError:
-            await context.message.channel.send(embed=get_user(context, context.message.author))
+            if not len(embeds):
+                raise DataNotFoundError()
+            
         except DataNotFoundError:
             await context.message.channel.send('**Error**: Sorry, No valid user specified.')
         else:
             await BotEmbedPaginator(context, embeds).run()
+
+    @commands.command(brief='Fetch details for the current user.')
+    async def me(self, context):
+        await context.message.channel.send(embed=get_user(context, context.message.author))
+
