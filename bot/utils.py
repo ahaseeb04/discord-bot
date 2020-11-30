f81194b506c71290205031473cb61d97d7ce5117
jphillip44 <jphillip44@gmail.com>
2020-11-29 22:48:16 -0500

    fixed timezone

--- bot/utils.py
+++ bot/utils.py
@@ -1,5 +1,8 @@
-import discord
 from datetime import datetime
+from pytz import timezone
+
+import discord
+
 
 from database_tools import engine, sql_to_df, redis_access
 from bot import config
@@ -18,10 +21,13 @@ def get_user(context, user):
     embed.set_author(name=user, icon_url=user.avatar_url)
     embed.set_thumbnail(url=user.avatar_url)
 
-    embed.add_field(name='Registered', value=user.created_at.strftime("%a %b %-d, %Y @ %-I:%M %p"), inline=False)
-    embed.add_field(name='Joined at', value=user.joined_at.strftime("%a %b %-d, %Y @ %-I:%M %p"), inline=False)
+    tz = timezone('US/Eastern')
+    frmt_str = "%a %b %-d, %Y @ %-I:%M %p"
+
+    embed.add_field(name='Registered', value=user.created_at.astimezone(tz).strftime(frmt_str), inline=False)
+    embed.add_field(name='Joined at', value=user.joined_at.astimezone(tz).strftime(frmt_str), inline=False)
     if (last_message := redis.hmget('users', str(user.id))[0] or df.last_message.get(str(user.id))) is not None:
-        last_message = datetime.strptime(last_message, "%Y-%m-%d")
+        last_message = datetime.strptime(last_message, "%Y-%m-%d").astimezone(tz)
         embed.add_field(name="Last message", value=last_message.strftime('%a %b %-d, %Y'), inline=False)
 
     if len(roles) > 0:
