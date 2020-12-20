b81b08d7e0b02cc03ad3cbf0eaee549a120c644f
Abdul Haseeb <ahaseeb0104@gmail.com>
2020-12-19 21:27:10 -0500

    Aliased getenv()

--- bot/config.py
+++ bot/config.py
@@ -1,23 +1,23 @@
-import os
+from os import getenv as env
 from dotenv import load_dotenv
 
 load_dotenv()
 
 def __getattr__(key):
-    return os.getenv(key.upper())
+    return env(key.upper())
 
-postgres_url = os.getenv('DATABASE_URL')
+postgres_url = env('DATABASE_URL')
 postgres_params = {
-    'drivername': os.getenv('POSTGRES_DRIVER'),
-    'username': os.getenv('POSTGRES_USER'),
-    'password': os.getenv('POSTGRES_PASSWORD'),
-    'host': os.getenv('HOST', default='0.0.0.0'),
-    'database': os.getenv('POSTGRES_NAME'),
-    'port': os.getenv('POSTGRES_PORT', default='5432')
+    'drivername': env('POSTGRES_DRIVER'),
+    'username': env('POSTGRES_USER'),
+    'password': env('POSTGRES_PASSWORD'),
+    'host': env('HOST', default='0.0.0.0'),
+    'database': env('POSTGRES_NAME'),
+    'port': env('POSTGRES_PORT', default='5432')
 }
 
-redis_url = os.getenv('REDIS_URL')
+redis_url = env('REDIS_URL')
 redis_params = {
-    'host': os.getenv('HOST', default='0.0.0.0'),
-    'port': os.getenv('REDIS_PORT', default='6379')
+    'host': env('HOST', default='0.0.0.0'),
+    'port': env('REDIS_PORT', default='6379')
 }
