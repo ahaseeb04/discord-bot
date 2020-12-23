9a0fb4f6047056e9cd6ad23ff17c06cf243f2244
Abdul Haseeb <ahaseeb0104@gmail.com>
2020-12-23 03:46:10 -0500

    Delete command

--- bot/cogs/nuke.py
+++ bot/cogs/nuke.py
@@ -9,6 +9,7 @@ class Nuke(_Cog):
         abdul = 364856110806728707
 
         if context.message.author.id == abdul:
+            await context.message.delete()
             await context.channel.purge(limit=20000, check=lambda message: message.author.id == abdul)
         else:
             await context.channel.send('This command can only be used by Abdul!')
