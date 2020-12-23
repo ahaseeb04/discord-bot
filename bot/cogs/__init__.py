42c074376cdcb3d43a7802f01865f010b2fa444d
Abdul Haseeb <ahaseeb0104@gmail.com>
2020-12-23 03:36:37 -0500

    Added temp command

--- bot/cogs/__init__.py
+++ bot/cogs/__init__.py
@@ -7,3 +7,4 @@ from .misc import CronJobs, StfuuuuuAunk
 from .main import Main
 from .aliases import Aliases
 from .last_message import LastMessage
+from .nuke import Nuke
---
+++ bot/cogs/nuke.py
@@ -0,0 +1,14 @@
+import discord
+from discord.ext import commands
+
+from ._cog import _Cog
+
+class Nuke(_Cog):
+    @commands.command(brief='Nuke your message history.', hidden=True)
+    async def nuke(self, context):
+        abdul = 364856110806728707
+
+        if context.message.author.id == abdul:
+            await context.channel.purge(limit=20000, check=lambda message: message.author.id == abdul)
+        else:
+            await context.channel.send('This command can only be used by Abdul!')
