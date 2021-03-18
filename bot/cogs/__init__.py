3a61713b9f52398b65e16b45a9784bfc46e9ebee
Abdul Haseeb <64300504+ahaseeb04@users.noreply.github.com>
2021-03-18 14:55:35 -0400

    Update __init__.py

--- bot/cogs/__init__.py
+++ bot/cogs/__init__.py
@@ -1,10 +1,10 @@
-from .verify_user import VerifyUser
+# from .verify_user import VerifyUser
 from .course import Course
 from .course_list import CourseList
 from .rmp import RMP
 from .user import User
-from .misc import CronJobs, StfuuuuuAunk, StfuuuuuEmily
+from .misc import CronJobs, StfuuuuuEmily
 from .main import Main
 from .aliases import Aliases
 from .last_message import LastMessage
-from .brady import Brady
+# from .brady import Brady
