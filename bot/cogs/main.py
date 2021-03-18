7f80397744531febc5545cfee3116ada1830f882
Abdul Haseeb <64300504+ahaseeb04@users.noreply.github.com>
2021-03-18 15:13:45 -0400

    Update main.py

--- bot/cogs/main.py
+++ bot/cogs/main.py
@@ -10,17 +10,17 @@ class Main(_Cog):
     async def on_ready(self):
         print("Ready!")
 
-    @_Cog.listener()
-    async def on_command_error(self, context, error):
-        pr = self.client.command_prefix
-        if context.message.channel.id == int(config.verification_channel) and context.message.content.lower().startswith(f'{pr}verify{pr}'):
-            await VerifyUser.verify(self, context)
-        else:
-            print(error)
+#     @_Cog.listener()
+#     async def on_command_error(self, context, error):
+#         pr = self.client.command_prefix
+#         if context.message.channel.id == int(config.verification_channel) and context.message.content.lower().startswith(f'{pr}verify{pr}'):
+#             await VerifyUser.verify(self, context)
+#         else:
+#             print(error)
 
-    @_Cog.listener()
-    async def on_message(self, message):
-        pr = self.client.command_prefix
-        if message.channel.id == int(config.verification_channel) and ''.join(message.content.lower().split()).startswith(f'verify{pr}'):
-            context = await self.client.get_context(message)
-            await VerifyUser.verify(self, context)
+#     @_Cog.listener()
+#     async def on_message(self, message):
+#         pr = self.client.command_prefix
+#         if message.channel.id == int(config.verification_channel) and ''.join(message.content.lower().split()).startswith(f'verify{pr}'):
+#             context = await self.client.get_context(message)
+#             await VerifyUser.verify(self, context)
